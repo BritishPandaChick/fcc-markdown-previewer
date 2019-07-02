5984c1b41e651341cbc41cc42a1ed7400a535ec1
BritishPandaChick <sbartley1080@gmail.com>
2019-07-01 22:27:10 -0400

    Create app. Add code to index.html, App.css, and App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let marked = require('marked');
+
+const firstMarkdown = `
+### Headers
+# Header 1
+## Header 2
+
+### Lists
+- list item one
+- list item two
+
+### Links
+[Free Code Camp](https://freecodecamp.org)
+
+### Text Decorations
+
+**bold**
+
+### Images
+![Cute Baby Panda](https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2018/babypandabor.jpg)
+
+### Blockquotes
+> To be, or not be. That is a stupid question.
+
+### Inline Code
+\`npm install create-react-app -g\`
+
+
+### Code Block
+\`\`\`
+function helloWorld() {
+  console.log("Hello World");
 }
+\`\`\`
+`;
+
+var renderer = new marked.Renderer()
+renderer.link = function(href, title, text) {
+  return `<a href=${href} target="_blank">${text}</a>`
+}
+
+marked.setOptions({
+  breaks: true,
+  renderer: renderer
+});
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      markdown: firstMarkdown
+    }
+  }
+
+  handleChange = e => this.setState({ markdown: e.target.value })
+
+  render() {
+    return (
+      <div>
+        <div className='container'>
+          <div className="left">
+            <textarea id="editor" value={this.state.markdown} onChange={this.handleChange}></textarea>
+          </div>
+          <div className="right">
+            <div id="preview" dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}}></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
 
 export default App;
