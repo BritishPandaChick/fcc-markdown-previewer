977d3cbfe6e8da8fa70842d36314081c05a177aa
BritishPandaChick <sbartley1080@gmail.com>
2022-06-12 22:37:59 -0400

    Add default markdown text for editor. Fix issues exporting app. Start adding styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
+import { marked } from "marked";
 import './App.css';
 
 function App() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState(`
+  # Header 1 
+  - list item one 
+  - list item two 
+
+  [Free Code Camp](https://freecodecamp.org)
+
+  **bold**
+
+  ## Header 2 
+  ![Cute Baby Panda](https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2018/babypandabor.jpg)
+
+  > To be, or not be. That is a stupid question.
+
+  \`npm install create-react-app -g\`
+
+  \`\`\`
+  function helloWorld() {
+    console.log("Hello World");
+  }
+  \`\`\`
+  `);
+
+  marked.setOptions({
+    breaks: true
+  });
 
   return <div className="App">
     <textarea
@@ -12,6 +38,13 @@ function App() {
       }}
       value={text}
     ></textarea>
-    <div id="preview">{text}</div>
+    <div
+      id="preview"
+      dangerouslySetInnerHTML={{
+        __html: marked(text),
+      }}
+    ></div>
   </div>
 }
+
+export default App;
